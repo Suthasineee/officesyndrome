a0144c7eb7a64ed6bff24cfd2874ecdc16978aa4
suthasineeSangchan <86451607+suthasineeSangchan21@users.noreply.github.com>
2025-09-12 23:32:54 +0700

    fix noti

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.office_syndrome"
+    namespace = "com.app.office_syndrome"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.office_syndrome"
+        applicationId = "com.app.office_syndrome"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
