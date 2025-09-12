11def4a2636e1a4eb0fa29de396770c023b139c4
Suthasineee <126067853+Suthasineee@users.noreply.github.com>
2025-09-12 17:27:20 +0700

    add notification

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,9 +6,17 @@
 // @dart = 3.8
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:video_player_android/video_player_android.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
+import 'package:flutter_local_notifications_linux/flutter_local_notifications_linux.dart';
+import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:shared_preferences_linux/shared_preferences_linux.dart';
+import 'package:shared_preferences_foundation/shared_preferences_foundation.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
+import 'package:path_provider_windows/path_provider_windows.dart';
+import 'package:shared_preferences_windows/shared_preferences_windows.dart';
 
 @pragma('vm:entry-point')
 class _PluginRegistrant {
@@ -16,6 +24,15 @@ class _PluginRegistrant {
   @pragma('vm:entry-point')
   static void register() {
     if (Platform.isAndroid) {
+      try {
+        SharedPreferencesAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         AndroidVideoPlayer.registerWith();
       } catch (err) {
@@ -27,6 +44,15 @@ class _PluginRegistrant {
 
     } else if (Platform.isIOS) {
       try {
+        SharedPreferencesFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         AVFoundationVideoPlayer.registerWith();
       } catch (err) {
         print(
@@ -36,7 +62,43 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isLinux) {
+      try {
+        LinuxFlutterLocalNotificationsPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`flutter_local_notifications_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        PathProviderLinux.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesLinux.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isMacOS) {
+      try {
+        SharedPreferencesFoundation.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_foundation` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         AVFoundationVideoPlayer.registerWith();
       } catch (err) {
@@ -47,6 +109,24 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isWindows) {
+      try {
+        PathProviderWindows.registerWith();
+      } catch (err) {
+        print(
+          '`path_provider_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        SharedPreferencesWindows.registerWith();
+      } catch (err) {
+        print(
+          '`shared_preferences_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     }
   }
 }
