33aea6fce9db41c5d5add3c3511bbd3ff03e4e19
suthasineeSangchan <86451607+suthasineeSangchan21@users.noreply.github.com>
2025-09-16 16:46:23 +0700

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.office_syndrome"
+    namespace = "com.application.office_syndrome"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -31,23 +31,30 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.office_syndrome"
+        applicationId = "com.application.office_syndrome"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 3
+        versionName = "1.0.3"
     }
 
      signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file('C:\Users\POTAE\Documents\GitHub\officesyndrome\release_key.jks')
-            storePassword = keystoreProperties["storePassword"] as String
-        }
+       create("release") {
+            val storeFilePath = keystoreProperties.getProperty("storeFile") ?: ""
+            if (storeFilePath.isNotEmpty()) {
+                storeFile = file(storeFilePath)
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+                enableV1Signing = true
+                enableV2Signing = true
+                enableV3Signing = true
+                enableV4Signing = true
+            }
     }
+     }
 
         buildTypes {
              getByName("release") {
@@ -63,7 +70,7 @@ android {
 }
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 flutter {
     source = "../.."
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Keep classes for flutter_local_notifications
+-keep class com.dexterous.flutterlocalnotifications.** { *; }
